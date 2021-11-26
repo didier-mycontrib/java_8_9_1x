657ad3564f460512d11233512748158693d02981
didierDefrance <didier@d-defrance.fr>
2021-11-26 12:10:09 +0100

    tp synthese

--- new_features/j15_16_17/src/main/java/tp/j15_16_17/Dto.java
+++ new_features/j15_16_17/src/main/java/tp/j15_16_17/Dto.java
@@ -14,5 +14,71 @@ public class Dto {
 			this(id,firstName,lastName,null);
 		}
 	};
+	
+	//... record LineRecord(...) en PHASE3 du Tp de Synthese
+	public record LineRecord(int x1,int y1,int x2,int y2,String lineColor) {
+		@Override
+		public String toString() {
+			return """
+					{ 
+					  "x1" : %d, 
+					  "y1" : %d,
+					  "x2" : %d,
+					  "y2" : %d,
+					  "lineColor" : "%s"
+					}
+					""".formatted(x1,y1,x2,y2,lineColor);
+		}
+	};
+	
+	//... record RectangleRecord(...) en PHASE3 du Tp de Synthese
+	public record RectangleRecord(int x1,int y1,int largeur,int hauteur,String fillColor) {
+		@Override
+		public String toString() {
+			return """
+					{ 
+					  "x1" : %d, 
+					  "y1" : %d,
+					  "largeur" : %d,
+					  "hauteur" : %d,
+					  "fillColor" : "%s"
+					}
+					""".formatted(x1,y1,largeur,hauteur,fillColor);
+		}
+	};
+		
+	//... record CircleRecord(...) en PHASE3 du Tp de Synthese
+	public record CircleRecord(int xc,int yc,int radius,String fillColor) {
+		@Override
+		public String toString() {
+			return """
+					{ 
+					  "xc" : %d, 
+					  "yc" : %d,
+					  "radius" : %d,
+					  "fillColor" : "%s"
+					}
+					""".formatted(xc,yc,radius,fillColor);
+		}
+	};
+		
+	//... record FigureRecord(...) en PHASE3 du Tp de Synthese
+	public record FigureRecord(String type,int x1,int y1,int x2,int y2,String color) {
+		@Override
+		public String toString() {
+			return """
+					{ 
+					  "type" : "%s", 
+					  "x1" : %d, 
+					  "y1" : %d,
+					  "x2" : %d,
+					  "y2" : %d,
+					  "color" : "%s"
+					}
+					""".formatted(type,x1,y1,x2,y2,color);
+		}
+	};
+
+		
 
 }
--- new_features/j15_16_17/src/main/java/tp/j15_16_17/SyntheseTestApp.java
+++ new_features/j15_16_17/src/main/java/tp/j15_16_17/SyntheseTestApp.java
@@ -1,10 +1,56 @@
 package tp.j15_16_17;
 
+import java.util.List;
+import java.util.function.Function;
+
+import tp.data.Figure;
+import tp.util.FigureUtil;
+
 public class SyntheseTestApp {
+
+	private List<Figure> listeFigures = FigureUtil.initSampleFigureList();
 
 	public static void main(String[] args) {
 		// Exercice de synthese (avec code à compléter)
-
+		SyntheseTestApp  syntheseTestApp = new SyntheseTestApp();
+		syntheseTestApp.selonTypeFigure();
+		syntheseTestApp.autresExperimentationsSelonInspirationDuMoment();
 	}
 
+	private void selonTypeFigure() {
+		for(Figure fig : listeFigures) {
+		/*	
+		//switch/case avec lambda expression
+		//PHASE1: afficher LIGNE (en francais) si fig.type vaut Figure.TYPE_LINE 
+		//             RECTANGLE si fig.type vaut Figure.TYPE_RECTANGLE
+        //             CERCLE (en francais)  si fig.type vaut Figure.TYPE_CIRCLE
+		//             TYPE DE FIGURE INCONNU sinon (par défaut)
+		switch(fig.getType()) {
+		    case Figure.TYPE_LINE -> System.out.println("LIGNE");
+		    case Figure.TYPE_RECTANGLE -> System.out.println("RECTANGLE");
+		    case Figure.TYPE_CIRCLE -> System.out.println("CERCLE");
+		    default -> System.out.println("TYPE DE FIGURE INCONNU");
+		}
+		*/
+		//PHASE2: via switch/case en tant qu'expression retournant valeur
+		//et pour chaque case :
+		//      fonctionalité de la PHASE1 ET
+		//      via yield , retourner une fonction de transformation adéquate
+		Function<Figure,Object> transformationFunction =
+				switch(fig.getType()) {
+				    case Figure.TYPE_LINE -> { System.out.println("LIGNE"); yield FigureUtil::figureToLine; }
+				    case Figure.TYPE_RECTANGLE ->  { System.out.println("RECTANGLE"); yield FigureUtil::figureToRectangle; }
+				    case Figure.TYPE_CIRCLE -> { System.out.println("CERCLE"); yield FigureUtil::figureToCircle; }
+				    default -> { System.out.println("TYPE DE FIGURE INCONNU"); yield FigureUtil::figureToUnknown; }
+				};
+				
+		Object resTransformation = transformationFunction.apply(fig);
+		System.out.println("resTransformation="+resTransformation);
+		
+		}
+	}
+	
+	private void autresExperimentationsSelonInspirationDuMoment() {
+	   //....
+	}
 }
